9fcd4e4e09ebc896aa277ebaba9b8d9717e0c95a
Moses Onche <onchemoses38@gmail.com>
2022-11-16 13:34:55 +0100

    Get command from prompt

---
+++ get_sigint.c
@@ -0,0 +1,11 @@
+nclude "main.h"
+
+/**
+ * get_sigint - Handle the crtl + c call in prompt
+ * @sig: Signal handler
+ */
+void get_sigint(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n(_B_) ", 5);
+}
